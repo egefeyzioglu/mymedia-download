4c687220d0c9b649fe135b37f5f5e33338080635
Ege Feyzioglu <egefeyzioglu@yahoo.com>
2023-12-15 03:25:08 -0500

    Resolves #2, resolves #3
    I really should be studying for finals right now but I don't wanna :(
    So I figured I would finally unfuck this tool

--- mymedia-downloader.py
+++ mymedia-downloader.py
@@ -1,23 +1,33 @@
 #! /usr/bin/env python3
 
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common import desired_capabilities
-from selenium.webdriver.common.keys import Keys
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-import time # sleep
-import json
-import os
 import ffmpeg
 import sys
+import re
+
+playlist_url = (
+        "https://stream.library.utoronto.ca:1935"
+        "/MyMedia/play/mp4:1/{}.mp4/playlist.m3u8"
+        )
+# This is pretty terrible performance-wise but honestly I don't care that much
+video_id_pattern = "(?<=embed/)|(?<=watch/)[0-9a-zA-Z]*"
 
 # Check if argc is correct
 if len(sys.argv) != 3:
-    print("Usage: py " + sys.argv[0] + " (file containing one URL per line) (output folder with trailing slash)")
+    print(
+            "Usage: py " +
+            sys.argv[0] +
+            (
+                " (file containing one URL per line) "
+                "(output folder with trailing slash)"
+            )
+            )
     exit()
 
-print("This will not work unless ffmpeg is installed and in your path. Read the GitHub page if you do not know how to do that.")
+print(
+        (
+            "This will not work unless ffmpeg is installed and in your path. "
+            "Read the GitHub page if you do not know how to do that.")
+        )
 yn = input("Is ffmpeg installed and in your path? [Y/n] ")
 if yn != "Y" and yn != "y":
     print("Please go do that and run me again.")
@@ -32,42 +42,10 @@ fp = open(sys.argv[1], "r")
 video_urls = fp.readlines()
 # Output path
 slash = '\\' if sys.platform == "win32" else '/'
-outpath = sys.argv[2] if sys.argv[2][-1] == slash else (sys.argv[2] + slash) # Add trailing back/slash if it isn't there
-
+# Add trailing back/slash if it isn't there
+outpath = sys.argv[2] if sys.argv[2][-1] == slash else (sys.argv[2] + slash)
 # We're done with this file
 fp.close()
-
-# Enable network logs to get true video URLs
-caps = DesiredCapabilities.CHROME
-caps['goog:loggingPrefs'] = {'performance': 'ALL'}
-
-# Launch Chrome with no console output (network logs not enabled while accepting user credentials)
-os.environ["WDM_LOG_LEVEL"] = "0"
-opts = webdriver.ChromeOptions()
-opts.add_argument("--log-level=3")
-driver = webdriver.Chrome(ChromeDriverManager().install(), service_log_path=os.devnull, chrome_options=opts)
-
-# Authenticate
-print("\n"*100) # Clear screen because ChromeDriverManager doesn't know how to shut up
-print("Please authenticate in the Chrome window")
-driver.get("https://play.library.utoronto.ca/login")
-while 'Welcome to' not in driver.page_source:
-    pass
-
-# Get cookies to stay logged in as we're switching to headless mode
-cookies = driver.get_cookies()
-# Then launch again in headless mode with no console output and set the old cookies")
-opts = webdriver.ChromeOptions()
-opts.add_argument("headless")
-opts.add_argument("--log-level=3")
-driver = webdriver.Chrome(ChromeDriverManager().install(), desired_capabilities=caps, chrome_options=opts, service_log_path=os.devnull)
-
-print("\n"*100) # Clear screen because ChromeDriverManager doesn't know how to shut up
-print("Authenticated. Collecting video URLs... ", end='')
-
-driver.get("https://play.library.utoronto.ca/login") # Browse to a MyMedia domain to be able to set cookies
-for cookie in cookies:
-    driver.add_cookie(cookie)
 
 # List to hold all the video  URLs
 playlists = []
@@ -76,26 +54,35 @@ titles = []
 
 # Get video URLs and titles
 for url in video_urls:
-    driver.get(url)
-    time.sleep(3) # Wait until page loads. Hopefully will be replaced with a non-janky method later
-
-    #Get video title
-    titles.append(driver.find_element(By.CSS_SELECTOR, "h2.chakra-heading").text)
-
-    #Get video URL
-    log = driver.get_log('performance')
-    for item_ in log:
-        item = json.loads(item_["message"])["message"]
-        if item["method"] == "Network.responseReceived":
-            if item["params"]["response"]["url"].endswith("playlist.m3u8"):
-                playlists.append(item["params"]["response"]["url"])
-
+    # Get video ID
+    video_id = re.search(video_id_pattern, url, flags=re.MULTILINE)
+    if video_id is None:
+        continue
+    video_id = video_id.group(0)
+    playlists.append(playlist_url.format(video_id))
+    # print(video_id)
+print("Read {} video URLs, extracted {} playlists.".format(
+    len(video_urls),
+    len(playlists)))
+print("")
+print("Reading video titles is currently not supported.")
+print("I'm going to fix this at some point but I need to study for finals")
+print("So the files are just going to be numbered.")
+titles = [str(x) for x in range(len(playlists))]
+print("")
+print("Also another thing -I promise I'll fix it after finals, but for now")
+print("you need to specify beforehand if you want to overwrite files in")
+print("the output directory. The file names will be:")
+print("\n".join([x+".mp4" for x in titles]))
+print("If these exist in the output directory, would you like them to")
+yn = input("be overwritten? [Y/n] ")
+overwrite_output = (yn == "Y" or yn == "y")
 # Download videos
-print("Done.\nStarting download...")
+print("Starting download...")
 for i, playlist in enumerate(playlists):
+    # print(playlist)
     stream = ffmpeg.input(playlist)
     stream = ffmpeg.output(stream, outpath + titles[i] + ".mp4", c="copy")
-    ffmpeg.run(stream)
-    print(".",end="")
-print("\n"*100 + "Done.")
-
+    ffmpeg.run(stream, overwrite_output=overwrite_output, quiet=True)
+    print(".", end="")
+print("\nDone.")
